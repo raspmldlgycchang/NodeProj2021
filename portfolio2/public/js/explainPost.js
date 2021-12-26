4a18e7eccb788e4f60a9fbc0f459564b5846c428
raspmldlgycchang <raspmldlgycchang8753@naver.com>
2021-12-26 13:24:45 +0900

    views폴더에서 깃애드하면views폴더 밖에 꺼는 add안됨 그래서 다시 public폴더꺼 더해줌 views폴더에서하고있으니까 상대경로 써주면됨 public폴더의 img랑 js파일들 두개 더함 at 2112261324

---
+++ portfolio2/public/js/explainPost.js
@@ -0,0 +1,9 @@
+var explainpost=[
+    {
+        title:'라즈베리파이초기설정_이미 vnc로 사용중이셔서 tightvnc설치하면 vnc사라져서& 모니터없이 설치하고 싶은데이것땜시 못한 경우 vnc초기설정 with 모바스템 or Putty',
+        contents:['hello', 'jo']
+    },{
+        title:'fd',
+        contents:['fdf','fdfds']
+    }
+]
